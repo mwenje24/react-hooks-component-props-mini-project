a704d710916308849d84303b49e285acef1beeb5
Mwenje <mwenje24@gmail.com>
2022-07-15 13:05:01 +0300

    created an about component and passed it in the app component with the blog data as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import blogData from "../data/blog";
+import About from "./About";
 import Header from "./Header";
 
 console.log(blogData);
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about}/>
     </div>
   );
 }
